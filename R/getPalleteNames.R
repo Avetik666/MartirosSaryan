89e4f5d28da9c900a2a90b8740dcf8005e179dae
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2018-08-09 02:24:17 +0400

    Added getPalleteNames function
    Added getPalleteNames function which returns all pallete names

---
+++ R/getPalleteNames.R
@@ -0,0 +1,17 @@
+#' Get Martiros Saryan paintings' pallete names
+#' @title Get Martiros Saryan Paintings Pallete names
+#' @return vector of pallete names
+#' @export
+
+getPalleteNames <- function() {
+  return(SaryanPalletes$Name)
+}
+
+
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
